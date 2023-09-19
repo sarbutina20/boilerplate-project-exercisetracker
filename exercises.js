c618eb9a6a8e027cea0f300236198b9c24d3cb80
sarbutina20 <sarbutina20@foi.hr>
2023-09-19 19:55:59 +0200

    Maknut .toDateString()

--- exercises.js
+++ exercises.js
@@ -47,6 +47,7 @@ class Exercises {
     }
   }
 
+  /*You can add from, to and limit parameters to a GET /api/users/:_id/logs request to retrieve part of the log of any user. from and to are dates in yyyy-mm-dd format. limit is an integer of how many logs to send back.*/
   async getExercises(req, res) {
     const id = req.params._id;
     const { from, to, limit } = req.query;
@@ -55,45 +56,48 @@ class Exercises {
     if (!user) {
       return res.status(400).json("Error: User not found");
     }
+    try {
+      Logs.findById(id).then((logs) => {
+        if (logs) {
+          logs.log = logs.log.map((logEntry) => ({
+            description: logEntry.description,
+            duration: logEntry.duration,
+            date: new Date(logEntry.date).toDateString(),
+          }));
 
-    Logs.findById(id).then((logs) => {
-      if (logs) {
-        logs.log = logs.log.map((logEntry) => ({
-          description: logEntry.description,
-          duration: logEntry.duration,
-          date: new Date(logEntry.date).toDateString(),
-        }));
+          if (from) {
+            const fromDate = new Date(from);
+            logs.log = logs.log.filter((logEntry) => {
+              const logDate = new Date(logEntry.date);
+              return logDate >= fromDate;
+            });
+          }
 
-        if (from) {
-          const fromDate = new Date(from);
-          logs.log = logs.log.filter((logEntry) => {
-            const logDate = new Date(logEntry.date);
-            return logDate >= fromDate;
+          if (to) {
+            const toDate = new Date(to);
+            logs.log = logs.log.filter((logEntry) => {
+              const logDate = new Date(logEntry.date);
+              return logDate <= toDate;
+            });
+          }
+
+          if (limit) {
+            logs.log = logs.log.slice(0, limit);
+          }
+
+          res.json({
+            _id: user._id,
+            username: user.username,
+            from: from ? new Date(from) : undefined,
+            to: to ? new Date(to) : undefined,
+            count: logs.log.length,
+            log: logs.log,
           });
         }
-
-        if (to) {
-          const toDate = new Date(to);
-          logs.log = logs.log.filter((logEntry) => {
-            const logDate = new Date(logEntry.date);
-            return logDate <= toDate;
-          });
-        }
-
-        if (limit) {
-          logs.log = logs.log.slice(0, limit);
-        }
-
-        res.json({
-          _id: user._id,
-          username: user.username,
-          from: from ? new Date(from).toDateString() : undefined, 
-          to: to ? new Date(to).toDateString() : undefined,
-          count: logs.log.length,
-          log: logs.log,
-        });
-      }
-    });
+      });
+    } catch (err) {
+      res.status(400).json("Error: " + err);
+    }
   }
 }
 
